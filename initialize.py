d5ae0aa6550bc0367dda2f9a5a1203d77b26e9ee
ysaty8hk-ctrl <ysaty8hk@gmail.com>
2025-10-30 02:17:22 +0900

    first commit.

--- initialize.py
+++ initialize.py
@@ -123,8 +123,10 @@ def initialize_retriever():
     
     # チャンク分割用のオブジェクトを作成
     text_splitter = CharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50,
+        #chunk_size=500,
+        #chunk_overlap=50,
+        chunk_size=ct.ct.CHUNK_SIZE,
+        chunk_overlap=ct.CHUNK_OVERLAP,
         separator="\n"
     )
 
@@ -135,8 +137,8 @@ def initialize_retriever():
     db = Chroma.from_documents(splitted_docs, embedding=embeddings)
 
     # ベクターストアを検索するRetrieverの作成
-    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 3})
-
+    #st.session_state.retriever = db.as_retriever(search_kwargs={"k": 3})
+    st.session_state.retriever = db.as_retriever(search_kwargs={"k": ct.RETRIEVER_K})
 
 def initialize_session_state():
     """
